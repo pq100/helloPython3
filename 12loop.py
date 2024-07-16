00a23784ea4787376e5775b90326f344223396ed
pq100 <system32bit@naver.com>
2024-07-16 10:41:06 +0900

    연습 문제 추가 break

--- 12loop.py
+++ 12loop.py
@@ -38,7 +38,6 @@ while i <= 50:
 
 # 1 ~ 10000사이 정수의 합을 출력
 # 단, 정수의 합이 12345678보다 계산을 중지
-
 sum = 0
 
 for i in range(1, 10000 + 1):
@@ -49,7 +48,6 @@ print(sum)
 
 # 1 ~ 10000사이 정수의 합을 출력
 # 단, 정수의 합이 12345678보다 계산을 중지 (while 문으로 작성)
-
 i = 1
 sum = 0
 while i < 10001:
@@ -60,8 +58,30 @@ while i < 10001:
 print(sum, i)
 
 
+# while 문을 이용해 1 ~ 100까지 정수 중 3과 8의 공배수와 최소 공배수를 출력
 
-# while 문을 이용해 1 ~ 100까지 정수 중 3과 7의 공배수와 최소 공배수를 출력
+# 1 ~ 100까지 정수 중 3과 8의 공배수와 최소 공배수를 출력
+result = ''
+
+for i in range(1, 100+1):
+    if i % 3 == 0 and i % 8 ==0:
+        result += f'{i} '
+
+print(result, f'[{3 * 8}]')
+
+
+
+# 삼각형의 넓이 구하기
+limitArea = 150     # 반복 중단 삼각형 너비
+width = 2
+height = 3
+i = 1
+
+while True:
+    area = ((width * i) * (height * i)) / 2
+    if area > limitArea: break
+    print(f'삼각형 너비 : {width * i} / {height * i} = {area}')
+    i += 1
 
 
 
@@ -70,7 +90,6 @@ print(sum, i)
 # '3' in str(36)
 # '6' in str(36)
 # '9' in str(12)
-
 i = 1
 while i < 100:
     jjak = ''
@@ -80,4 +99,51 @@ while i < 100:
     if i == 33 or i == 66 or i == 99: jjak += ' 짝!'
     print(i, jjak)
     i += 1
+
+
+# 열차 교차시간 알아보기
+trainA = 10
+trainB = 25
+trainC = 30
+min = 1
+
+
+# for min in range(1, 540 + 1):  <<< 이게 밑의 while문과 같음
+
+while min < 541:
+    # if 문들
+
+    if min % trainA == 0 and min % 25 == 0:  # 10분 간격
+        print(f'{hour}시 {min}분 : A - B 교차!')
+
+    elif min % trainB == 0 and min % trainC == 0: # 50분 간격
+        hour = 9 + min // 60
+        min = min % 60
+        print(f'{hour}시 {min}분 : B - C 교차!')
+
+    elif min % trainC == 0 and min % trainA == 0: # 30분 간격
+        hour = 9 + min // 60
+        min = min % 60
+        print(f'{hour}시 {min}분 : C - A 교차!')
+
+    min += 1
+
+
+# 로그인 기능 만들기
+cntLogin = 1
+
+while True:
+    passwd = input('관리자 암호를 입력하세요. ')
+
+    if passwd == 'hanbitac':
+        isLogin = True
+        print('로그인 되었습니다!')
+        break
+    else:
+        print('암호를 다시 입력하세요!')
+
+    if cntLogin < 5: cntLogin += 1
+    else:
+        print('로그인 실패! 횟수 초과!')
+        break
 
