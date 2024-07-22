304c903f39aec0c9e4cfb8c82a1dec1ba8372243
pq100 <system32bit@naver.com>
2024-07-22 12:14:23 +0900

    성적 처리 프로그램 v7 - 데이터베이스 - 수정 : 학생 번호로 조회

--- snicuz/sungjukv7.py
+++ snicuz/sungjukv7.py
@@ -1,6 +1,5 @@
 import sqlite3
 
-
 def displayMenu():
     main_menu = '''
     =========================
@@ -18,14 +17,10 @@ def displayMenu():
     menu = input('=> 메뉴를 선택하세요 : ')
     return menu
 
-# 성적 데이터 관련 변수 선언
-sjs = [ ['일지매',99,87,65,251,83.7,'우'] ]
-
-
 # 성적 데이터를 입력 받는 함수
 def readSungJuk():
     sj = []
-    cnt = len(sjs)
+    cnt = getTotalSungJuk()
     sj.append(input(f'{cnt}번 학생 이름은? '))
     sj.append(int(input(f'{cnt}번 학생 국어는? ')))
     sj.append(int(input(f'{cnt}번 학생 영어는? ')))
@@ -35,42 +30,8 @@ def readSungJuk():
 
 # 입력받은 성적 데이터를 처리하고 테이블에 저장
 def addSungJuk(sj):
-    sj.append(sj[1] + sj[2] + sj[3])
-    sj.append(sj[4] / 3)
-    grd = '수' if (sj[5] >= 90) else \
-        '우' if (sj[5] >= 80) else \
-            '미' if (sj[5] >= 70) else \
-                '양' if (sj[5] >= 60) else '가'
-    sj.append(grd)
-    sjs.append(sj)
-
-
-# 학생 이름으로 성적 데이터 조회 후 출력
-def showOneSungJuk():
-    # name = input('조회할 학생 이름은? ')
-    sjno = input('조회할 학생 번호는? ')
-    result = '데이터가 존재하지 않아요!!'
-    sj = readOneSungJuk(sjno)
-    if sj: # 조회한 데이터가 존재한다면
-        result = (f'번호: {sj[0]}, 이름: {sj[1]}, 국어: {sj[2]}, 영어: {sj[3]}, 수학: {sj[4]}\n'
-                  f'총점: {sj[5]}, 평균: {sj[6]:.1f}, 학점: {sj[7]}, 등록일: {sj[8]}')
-    print(result)
-
-# ----------------------------------
-
-# 성적 데이터 총 갯수 조회
-def getTotalSungJuk():
-    sql = 'select count(sjno) + 1 total from sungjuk'
-    cnt = 0
-    conn = sqlite3.connect('db/python/db')
-    cursor = conn.cursor()
-    cursor.execute(sql)
-    rs = cursor.fetchall()
-    for r in rs:
-        cnt = r[0] + 1
-    cursor.close()
-    conn.close()
-    return cnt
+    computeSungJuk(sj)
+    newSungJuk(sj)
 
 
 # 테이블에 저장된 성적 데이터들 중 기본 데이터만 모아서 출력
@@ -78,11 +39,37 @@ def showSungJuk():
     result = ''
     sjs = readAllSungJuk()
     for sj in sjs:
-        result += f'번호: {sj[0]}, 이름: {sj[1]}, 국어: {sj[2]}, 영어: {sj[3]}, 수학: {sj[4]}, 둥록일: {sj[5]}\n'
+        result += f'번호: {sj[0]}, 이름: {sj[1]}, 국어: {sj[2]}, 영어: {sj[3]}, 수학: {sj[4]}, 등록일: {sj[5]}\n'
     print(result)
 
+# 학생 이름으로 성적데이터 조회후 출력
+def showOneSungJuk():
+    # name = input('조회할 학생 이름은? ')
+    sjno = input('조회할 학생 번호는? ')
+    result = '데이터가 존재하지 않아요!!'
+    sj = readOneSungJuk(sjno)
+    if sj:   # 조회한 데이터가 존재한다면
+        result = (f'번호: {sj[0]}, 이름: {sj[1]}, 국어: {sj[2]}, 영어: {sj[3]}, 수학: {sj[4]}\n'
+                  f'총점: {sj[5]}, 평균: {sj[6]:.1f}, 학점: {sj[7]}, 등록일: {sj[8]}')
+    print(result)
 
-# 입력한 성적데이터에 대해 성적 처리하는 함수
+# ------------------------------
+
+# 성적 데이터 총 갯수 조회
+def getTotalSungJuk():
+    sql = 'select count(sjno) + 1 total from sungjuk'
+    cnt = 0
+    conn = sqlite3.connect('db/python.db')
+    cursor = conn.cursor()
+    cursor.execute(sql)
+    rs = cursor.fetchall()
+    for r in rs:
+        cnt = r[0]
+    cursor.close()
+    conn.close()
+    return cnt
+
+# 입력한 성적데이터에 대해 성적처리하는 함수
 def computeSungJuk(sj):
     sj.append(sj[1] + sj[2] + sj[3])
     sj.append(sj[4] / 3)
@@ -91,26 +78,23 @@ def computeSungJuk(sj):
             '미' if (sj[5] >= 70) else \
                 '양' if (sj[5] >= 60) else '가'
     sj.append(grd)
-    sjs.append(sj)
 
-
-# 처리된 성적 데이터를 테이블에 저장
+# 처리된 성적데이터를 테이블에 저장
 def newSungJuk(sj):
     sql = 'insert into sungjuk (name, kor, eng, mat, tot, avg, grd) \
-           values ''(?,?,?,?, ?,?,?);'
+           values (?,?,?,?, ?,?,?)'
     conn = sqlite3.connect('db/python.db')
     cursor = conn.cursor()
-    params = (sj[0], sj[1], sj[2], sj[3], sj[4], sj[5], sj[6])
+    params = (sj[0],sj[1],sj[2],sj[3],sj[4],sj[5],sj[6])
     cursor.execute(sql, params)
     print(cursor.rowcount, '건의 데이터 추가됨!')
     conn.commit()
     cursor.close()
     conn.close()
 
-
 # 성적 데이터 전체 조회
 def readAllSungJuk():
-    sql = 'select name,kor,eng,mat,substr(regdate, 0,11) regdate from sungjuk'
+    sql = 'select sjno,name,kor,eng,mat,substr(regdate,0,11) regdate from sungjuk'
     conn = sqlite3.connect('db/python.db')
     cursor = conn.cursor()
     cursor.execute(sql)
@@ -130,3 +114,7 @@ def readOneSungJuk(sjno):
     cursor.close()
     conn.close()
     return sj
+
+
+
+
